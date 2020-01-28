152b8b611df8ace1429dcac83a6528496b865353
htr-volker <htr.volker@gmail.com>
2020-01-28 16:16:35 +0000

    css fix 6

--- server/application/__init__.py
+++ server/application/__init__.py
@@ -4,6 +4,6 @@ Main flask file to be run to start the app
 from flask import Flask, render_template
 
 app = Flask(__name__)
-app.static_folder = 'application/static'
+app.static_folder = 'static'
 
 from application import routes
