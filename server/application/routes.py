82b066134cd2408fccc3eb5ae72e352d347e96fe
htr-volker <htr.volker@gmail.com>
2020-01-28 12:41:56 +0000

    machine hostname is now displayed on the homepage

--- server/application/routes.py
+++ server/application/routes.py
@@ -4,6 +4,7 @@ Python file that handles hyperlink routing within the site
 from application import app
 from flask import render_template, redirect, url_for, request
 import requests
+from os import getenv
 
 # Route to home page
 @app.route('/', methods = ["GET"])
@@ -11,4 +12,5 @@ def home():
     response = requests.get("https://l5iqv5z489.execute-api.eu-west-2.amazonaws.com/test/get-name")
     package = response.json()
     name = f"{package['firstname']} {package['surname']}"
-    return render_template('index.html', title = 'Home', name = name)
+    machine = getenv("HOSTNAME")
+    return render_template('index.html', title = 'Home', name = name, machine = machine)
